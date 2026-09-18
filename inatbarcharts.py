d36e8738b3cf56843c77ae19e1a8c496b3026e77
Joshua Lucas Johnson <124935024+OleaginousHemispingus@users.noreply.github.com>
2026-09-17 20:37:18 -0400

    Update inatbarcharts.py

--- inatbarcharts.py
+++ inatbarcharts.py
@@ -313,10 +313,9 @@ def find_observations(taxon: int, place: int, start: str, end: str):
 
 	df_list = df_grouped.partition_by("group_id", include_key=True)
 
-	st.write(len(date_starts))
 
 	for x in range(1, len(date_starts)):
-
+		st.write(x-1)
 		try:
 			ourdf = df_list[x-1]
 			thisdate = ourdf.item(0,"group_id")
