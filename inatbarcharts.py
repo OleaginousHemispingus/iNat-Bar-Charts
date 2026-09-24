b2eb98bfe9a8f221891491e1c21ba219c9fd2e2a
Joshua Lucas Johnson <124935024+OleaginousHemispingus@users.noreply.github.com>
2026-09-23 20:02:40 -0400

    Update heatmap title based on user month input

--- inatbarcharts.py
+++ inatbarcharts.py
@@ -370,7 +370,11 @@ else:
 	fig, ax = plt.subplots(figsize=(16, esttime))
 sns.heatmap(df_pd, cmap="Purples", linewidths=0.2, linecolor='gray', vmax=absolute_max, ax=ax)
 
-ax.set_title(f"Frequency of {our_name} in {placename}", fontsize=15)
+if usermonth:
+	ax.set_title(f"Frequency of {our_name} in {placename} in {usermonth}", fontsize=15)
+else:
+	ax.set_title(f"Frequency of {our_name} in {placename}", fontsize=15)
+
 ax.set_xlabel("Week", fontsize=15)
 ax.set_ylabel(f"{Rank}", fontsize=15)
 
